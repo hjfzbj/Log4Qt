c7299cfc455fea48283feb9cb2cb40c63197280c
Andreas Bacher <andreas.bacher@meon-medical.com>
2026-04-01 09:37:56 +0200

    clean up build dir

--- build/examples/propertyconfigurator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Workspace/log4qt/examples/propertyconfigurator
-# Build directory: C:/Workspace/log4qt/build/examples/propertyconfigurator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/log4qt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Workspace/log4qt/src/log4qt
-# Build directory: C:/Workspace/log4qt/build/src/log4qt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
